99b1333ff8de0b8c81619f9524205c7d8bef9a39
trandung2k1 <trandungitksnb2001@gmail.com>
2023-04-16 22:23:29 +0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const Koa = require('koa');
+const dotenv = require('dotenv');
+dotenv.config();
+const helmet = require('koa-helmet');
+const morgan = require('koa-morgan');
+const { koaBody } = require('koa-body');
+const cors = require('@koa/cors');
+const colors = require('colors');
+const fs = require('fs');
+const isProduction = process.env.NODE_ENV === 'production';
+const accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {
+    flags: 'a',
+});
+const port = process.env.PORT || 4000;
+const app = new Koa();
+app.use(koaBody());
+app.use(isProduction ? morgan('combined', { stream: accessLogStream }) : morgan('tiny'));
+app.use(cors());
+app.use(helmet());
+app.use((ctx) => {
+    ctx.body = {
+        message: 'Welcome to server 👋👋',
+    };
+});
+
+app.listen(port, () => {
+    console.log(colors.green(`Server listening http://localhost:${port}`));
+});
